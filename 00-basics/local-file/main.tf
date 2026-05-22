ea279cc840b4a7bb0dd63d83260e9d5fa8d88af1
Tommaso Di Marcello <tommaso.dimarcello@which.co.uk>
2026-05-22 14:51:09 +0100

    00-basics

--- 00-basics/local-file/main.tf
+++ 00-basics/local-file/main.tf
@@ -1,6 +1,6 @@
 # This exercise creates a local file so you can learn Terraform safely.
 resource "local_file" "hello" {
   # Content written into the file.
-  content  = "Hello from Terraform basics!"
+  content  = "Daje forte co Infra as code! Imparare"
   filename = "${path.module}/hello.txt"
 }
